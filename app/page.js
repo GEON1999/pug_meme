8af3ab4a0cf3e30e1bf3f80c24b9225afd9190f1
geon <phgst@playdonut.com>
2024-03-27 12:17:14 +0900

    change discord address

--- app/page.js
+++ app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
             <nav className="fixed text-5xl  mt-10 text-red-600 flex mr-[120vh]">
                 <Link target={"_blank"} href="https://twitter.com/pug_smokin" data-text="Twitter" className="left-[40vh]">Twitter</Link>
                 <Link target={"_blank"} href="https://t.me/smokin_pug" data-text="Telegram" className="left-[80vh]">Telegram</Link>
-                <Link target={"_blank"} href="https://birdeye.so/token/EaR4ooS5yJ1DxvRPoyVXTYoTBekDhZimjonJ8V1PZk6L?chain=solana" data-text="Buy" className="left-[120vh]">Buy Now</Link>
-                <Link target={"_blank"} href="https://birdeye.so/token/EaR4ooS5yJ1DxvRPoyVXTYoTBekDhZimjonJ8V1PZk6L?chain=solana" data-text="Discord" >Chart</Link>
+                <Link target={"_blank"} href="https://birdeye.so/token/EaR4ooS5yJ1DxvRPoyVXTYoTBekDhZimjonJ8V1PZk6L?chain=solana" data-text="Buy" className="left-[120vh]">Buy</Link>
+                <Link target={"_blank"} href="https://discord.gg/DwHepbHS" data-text="Discord" >Discrod</Link>
             </nav>
 
 
